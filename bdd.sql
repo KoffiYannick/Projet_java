562c55a4d35cf76834d880d0b4d925799f7d8c2a
KoffiYannick <bassinayannick@yahoo.fr>
2024-01-15 16:35:09 +0100

    Commit pour les creations de tables aussi

--- bdd.sql
+++ bdd.sql
@@ -1,1 +1,16 @@
 create database java;
+CREATE TABLE salle (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    batiment VARCHAR(255),
+    etage INT,
+    numero INT
+);
+CREATE TABLE reservation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_salle INT,
+    FOREIGN KEY (id_salle) REFERENCES salle(id),
+    responsable VARCHAR(255),
+    promo VARCHAR(255),
+    date_debut DATETIME,
+    date_fin DATETIME
+);
